14ab55ef82a73b4e4248a62c66520d3eb996ddb3
janferdinand <164619505+janferdinand@users.noreply.github.com>
2024-03-23 13:36:01 +0100

    Test OTA update final

--- webApp.py
+++ webApp.py
@@ -15,7 +15,7 @@ def web_page():
   <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
   h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
   border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server update 7000</h1> 
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server update final</h1> 
   <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
   <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
   return html

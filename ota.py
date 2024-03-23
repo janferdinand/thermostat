b5c4508976dd6e1af1d0e0bc491e8786242d8291
janferdinand <164619505+janferdinand@users.noreply.github.com>
2024-03-23 13:19:10 +0100

    Test OTA update5000

--- ota.py
+++ ota.py
@@ -95,7 +95,7 @@ class OTAUpdater:
         print('Updating device...', end='')
 
         # Overwrite the old code.
-        os.rename('latest_code.py', self.filename)  
+        os.replace('latest_code.py', self.filename)  
 
         # Restart the device to run the new code.
         print("Restarting device... (don't worry about an error message after this")

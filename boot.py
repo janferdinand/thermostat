3ae97d492a8b938336e787a0a49ba1694028e23f
janferdinand <164619505+janferdinand@users.noreply.github.com>
2024-03-23 10:22:35 +0100

    first commit

---
+++ boot.py
@@ -0,0 +1,33 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+# Complete project details at https://RandomNerdTutorials.com
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'Bahnhof-2.4G-EKXLCM'
+password = '26261954'
+
+wlan = network.WLAN(network.STA_IF) # create station interface
+wlan.active(True)       # activate the interface
+wlan.ifconfig(("192.168.1.187", "255.255.255.0", "192.168.1.1", "192.168.1.1"))
+wlan.connect(ssid, password) # connect to an AP
+
+while wlan.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(wlan.ifconfig())
+
+led = Pin(2, Pin.OUT)
